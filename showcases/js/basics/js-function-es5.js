82cb51952a8f1da40f1d1e7ae0384c0780a23bd2
puddlejumper26 <xiang.wu@hotmail.de>
2019-08-05 02:12:30 +0200

    Update

---
+++ showcases/js/basics/js-function-es5.js
@@ -0,0 +1,21 @@
+var jsFuncEs5={
+
+//****************************字符串****************************/
+    //去除空格  type 1-所有空格  2-前后空格  3-前空格 4-后空格
+    //trim('  1235asd',1)
+    //result：1235asd
+
+    trim:function(str,type){
+        switch(type){
+            case 1:
+                return str.replace(/\s+/g,"");
+            case 2:
+                return str.replace()
+        }
+    }
+
+
+
+
+
+}
